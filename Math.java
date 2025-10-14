336ae10fb48317ec3b86d6d28f4935ba9e2355ad
Kamalpreet <kamalpreetsingh.ks17@gmail.com>
2025-10-14 12:59:45 +0100

    Addded multiplication function

---
+++ Math.java
@@ -0,0 +1,14 @@
+//Math Functions
+public class Math {
+
+
+    // Multiply Function (kamalpreet singh)
+    public int multiply (int number1, int number2)
+     {
+        return number1 * number2;
+
+    }
+
+
+
+}
