77a6960f9d1792fb1062d95e31d2ac759c7d2a9d
Benjamin Lee <benlee007@live.co.uk>
2025-11-18 12:39:21 +0000

    Merge branch 'main' into develop

---
+++ branchMath.java
@@ -0,0 +1,34 @@
+//New Math functions, move old functions in a branch and merge.
+
+public class Math {
+        /*
+    Developer: Benjamin Lee
+    University ID: 230134175
+    Function: This function takes two inputs as integers, the first number as the base and the second as the coefficient.
+        It then returns the base (num1) raised to by a power of the coefficient (num2).
+    */
+    public int Power(int num1, int num2) 
+    {
+        return Math.pow(num1,num2)
+    }
+    //modulo function (Benjamin Lee)
+    public int mod(int num1, int num2)
+    {
+        return num1%num2;
+    }
+
+
+// Minimum of two values (Mihsan)
+    public int min(int num1, int num2) {
+        if (num1 < num2) {
+            return num1;
+        } else-if {
+            return num2;
+        }
+    }
+
+    //Divide two values (Cherechi) test
+public int divide(int num1, int num2) {
+    return num1 / num2;
+}
+}
