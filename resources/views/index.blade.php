418396a0f45f0081ad2ef3e27aa4d7ece5dbe9dc
Kamalpreet <kamalpreetsingh.ks17@gmail.com>
2025-11-23 12:31:22 +0000

    basic pages for the website

---
+++ resources/views/index.blade.php
@@ -0,0 +1,46 @@
+<!-- -->
+<!DOCTYPE html>
+<!--lang specifies the language of the content-->
+<html lang="en">
+
+    <!--head tag is a container for metadata-->
+    <head>
+        <!--encoding type-->
+        <meta charset="UTF-8">
+        <!--for proper scaling-->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!--title page-->
+        <title>Homepage</title>
+        <!--Favicon-->
+        <link rel="icon" type="image/x-icon" href="">
+        <!--link to Bootstrap css file-->
+        <link rel="stylesheet" href="">
+        <!--link to custom css style file--> 
+        <link rel="stylesheet" href="">
+    </head>
+
+    <body>
+        <!--Heading-->
+        <h1>Homepage</h1>
+
+        <!--Navbar-->
+        <header> 
+            <nav>
+                <a href="/">Home</a>
+                <a href="/shop">Shop</a>
+                <a href="/basket">Basket</a>
+                <a href="/customerSupport">Customer Support</a>
+                <a href="/aboutUs">About Us</a>
+                <a href="/login">Login</a>
+                <a href="/signUp">Login</a>
+            </nav>
+        </header>
+
+
+
+
+        <!--java script connection-->
+        <script src=""></script>
+   </body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,29 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('aboutUs', function () {
+    return view('aboutUs');
+});
+
+Route::get('basket', function () {
+    return view('basket');
+});
+
+Route::get('customerSupport', function () {
+    return view('customerSupport');
+});
+
+Route::get('login', function () {
+    return view('login');
+});
+
+Route::get('shop', function () {
+    return view('shop');
+});
+
+Route::get('signUp', function () {
+    return view('signUp');
 });
