6da76aeb18362fc2b9baf772242b910ff6300f6a
Benjamin Lee <benlee007@live.co.uk>
2025-12-01 23:59:12 +0000

    reformatting default Laravel users table + swapping default database to mysql

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,11 +12,14 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->id('userID');
+            $table->int('addressID');
+
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email', 100)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone', 25);
             $table->rememberToken();
             $table->timestamps();
         });
