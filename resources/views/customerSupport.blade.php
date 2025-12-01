ef594be8b2674c13039c9e5a709da8be73e183e6
Kamalpreet <kamalpreetsingh.ks17@gmail.com>
2025-12-01 17:03:53 +0000

    Added a hero section

--- resources/views/customerSupport.blade.php
+++ resources/views/customerSupport.blade.php
@@ -12,8 +12,21 @@
         Customer Support Page
     </x-slot:title>
 
-    <!--heading-->
-    <h1>Welcome to the customer support page</h1>
+    
+    <!--Hero Section-->
+    <div class="hero bg-base-200 h-100 border border-gray-300 shadow-sm">
+    <div class="hero-content text-center">
+        <div class="max-w-md">
+        <h1 class="text-5xl font-bold">Hi there!</h1>
+        <p class="py-6 text-lg">
+            How can we help you today? 
+        </p>
+        <p class="text-lg">
+            You can find answers to the common questions in the FAQs section or contact us directly using form below. 
+        </p>
+    </div>
+    </div>
+
 
 </x-layout>
 
