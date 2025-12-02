d6e639d7aabcb91fce9cd76245172b74f42439b0
Benjamin Lee <benlee007@live.co.uk>
2025-12-02 01:10:19 +0000

    Added User Accessability Table

---
+++ app/Models/UserAccessability.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserAccessability extends Model
+{
+    /** @use HasFactory<\Database\Factories\UserAccessabilityFactory> */
+    use HasFactory;
+}
