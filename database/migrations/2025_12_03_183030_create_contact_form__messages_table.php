38b7c1842306f72469dc2ff2748d940a164c82ee
Kamalpreet <kamalpreetsingh.ks17@gmail.com>
2025-12-03 18:47:46 +0000

    created a table for the contact form messages

---
+++ database/migrations/2025_12_03_183030_create_contact_form__messages_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contact_form__messages', function (Blueprint $table) {
+            
+            // Table ID column
+            $table->id();
+
+            // Syntax for creating columns: $table->Datatype("ColumnName")->FutherModifiers();
+            
+            // Name column
+            $table->string('name');
+
+            // Email column
+            $table->string('email');
+
+            // Phone Number column
+            // nullable makes it optional
+            $table->string('mobile_number')->nullable();
+
+            // Order Number column
+            $table->string('order_number');
+
+            // Message column
+            $table->text('message');
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contact_form__messages');
+    }
+};
