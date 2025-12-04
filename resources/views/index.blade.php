198b9eb9d601ad3129609f3adacc90edccc3ac1b
Kamalpreet <kamalpreetsingh.ks17@gmail.com>
2025-12-04 13:56:23 +0000

    Added links to shop on homepage images

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,28 +15,28 @@
     <!--Testing new carousel-->
     <div class="carousel w-full border border-gray-300 shadow-sm">
     <div id="slide1" class="carousel-item relative w-full">
-        <img src="{{ asset('images/slide1.png') }}" class="w-full" />
+        <a href="/shop"> <img src="{{ asset('images/slide1.png') }}" class="w-full block" /></a>
         <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
         <a href="#slide4" class="btn btn-circle">❮</a>
         <a href="#slide2" class="btn btn-circle">❯</a>
         </div>
     </div>
     <div id="slide2" class="carousel-item relative w-full">
-        <img src="{{ asset('images/slide2.png') }}" class="w-full" />
+        <a href="/shop"> <img src="{{ asset('images/slide2.png') }}" class="w-full block" /></a>
         <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
         <a href="#slide1" class="btn btn-circle">❮</a>
         <a href="#slide3" class="btn btn-circle">❯</a>
         </div>
     </div>
     <div id="slide3" class="carousel-item relative w-full">
-        <img src="{{ asset('images/slide3.png') }}" class="w-full" />
+        <a href="/shop"> <img src="{{ asset('images/slide3.png') }}" class="w-full block" /></a>
         <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
         <a href="#slide2" class="btn btn-circle">❮</a>
         <a href="#slide4" class="btn btn-circle">❯</a>
         </div>
     </div>
     <div id="slide4" class="carousel-item relative w-full">
-        <img src="{{ asset('images/slide4.png') }}" class="w-full" />
+        <a href="/shop"> <img src="{{ asset('images/slide4.png') }}" class="w-full block" /></a>
         <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
         <a href="#slide3" class="btn btn-circle">❮</a>
         <a href="#slide1" class="btn btn-circle">❯</a>
@@ -55,7 +55,7 @@
         <!--product-->
         <div class="card bg-base-100 w-96 shadow-sm">
             <figure>
-                <a href="#"> <img src="{{ asset('images/grid1.png') }}" alt="product" /> </a>
+                <a href="/shop"> <img src="{{ asset('images/grid1.png') }}" alt="product" /> </a>
             </figure>
             <div class="bg-neutral card-body items-center py-2 rounded-b-sm">
                 <p class="font-bold text-lg text-white">MATRIX TEE</p>
@@ -65,7 +65,7 @@
         <!--product-->
         <div class="card bg-base-100 w-96 shadow-sm">
             <figure>
-                <a href="#"> <img src="{{ asset('images/grid2.png') }}" alt="product" /> </a>
+                <a href="/shop"> <img src="{{ asset('images/grid2.png') }}" alt="product" /> </a>
             </figure>
             <div class="bg-neutral card-body items-center py-2 rounded-b-sm">
                 <p class="font-bold text-lg text-white">BATTLE TEE</p>
@@ -75,7 +75,7 @@
         <!--product-->
         <div class="card bg-base-100 w-96 shadow-sm">
             <figure>
-                <a href="#"> <img src="{{ asset('images/grid3.png') }}" alt="product" /> </a>
+                <a href="/shop"> <img src="{{ asset('images/grid3.png') }}" alt="product" /> </a>
             </figure>
             <div class="bg-neutral card-body items-center py-2 rounded-b-sm">
                 <p class="font-bold text-lg text-white">BATTLE TEE</p>
