34d42ff59c81978ed50da62c8412060813f8c856
Ayomide Adewuyi <adewuyiayomide26@gmail.com>
2025-12-05 04:47:01 +0000

    Revised code

--- resources/views/forgotPassword.blade.php
+++ resources/views/forgotPassword.blade.php
@@ -6,21 +6,33 @@
 --}}
 
 <x-layout>
-@vite('resources/css/login-style.css')
-    <!--title page-->
+    @vite('resources/css/login-style.css')
+
     <x-slot:title>
         Forgot Password
     </x-slot:title>
+
     <div class="wrapper">
-        <form action="sendPasswordReset.blade.php">
+        <form action="{{ route('password.email') }}" method="POST">
+            @csrf
+
             <h1>Forgot your Password?</h1>
             <h3>Enter your email for a link to reset it!</h3>
+
             <div class="input-box">
-                <input type="email" placeholder="Email" required>
-                <i class='bx  bx-user'></i> 
+                <input type="email" name="email" placeholder="Email" required>
+                <i class='bx bx-user'></i>
             </div>
+
+            @error('email')
+                <p style="color:red; font-size:14px;">{{ $message }}</p>
+            @enderror
+
+            @if(session('success'))
+                <p style="color:lightgreen; font-size:14px;">{{ session('success') }}</p>
+            @endif
+
             <button type="submit" class="btn">Send</button>
         </form>
     </div>
-
-    </x-layout>
+</x-layout>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -7,30 +7,45 @@
 
 <x-layout>
 @vite('resources/css/login-style.css')
-    <!--title page-->
-    <x-slot:title>
-        Login
-    </x-slot:title>
-    <div class="wrapper">
-        <form action="">
-            <h1>Login</h1>
-            <div class="input-box">
-                <input type="email" placeholder="Email" required>
-                <i class='bx  bx-user'></i> 
-            </div>
-            <div class="input-box">
-                <input type="password" placeholder="Password" required>
-                <i class='bx  bx-lock'></i> 
-            </div>
-            <div class="remember-forgot">
-                <label><input type="checkbox">Remember Me</label>
-                <a href="/forgotPassword">Forgot Password?</a>
-            </div>
-            <button type="submit" class="btn">Login</button>
-            <div class="register-link">
-                <a href="/register">Don't have an account?</a>
-            </div>
-        </form>
-    </div>
 
-    </x-layout>
+<x-slot:title> Login </x-slot:title>
+
+<div class="wrapper">
+    <form action="{{ route('login.submit') }}" method="POST">
+        @csrf
+
+        <h1>Login</h1>
+
+        {{-- Error messages --}}
+        @if ($errors->any())
+            <div class="alert alert-error" style="color: #ff8080; margin-bottom: 10px;">
+                @foreach ($errors->all() as $error)
+                    <p>• {{ $error }}</p>
+                @endforeach
+            </div>
+        @endif
+
+        <div class="input-box">
+            <input type="email" name="email" placeholder="Email" required>
+            <i class='bx bx-user'></i>
+        </div>
+
+        <div class="input-box">
+            <input type="password" name="password" placeholder="Password" required>
+            <i class='bx bx-lock'></i>
+        </div>
+
+        <div class="remember-forgot">
+            <label><input type="checkbox" name="remember">Remember Me</label>
+            <a href="/forgot-password">Forgot Password?</a>
+        </div>
+
+        <button type="submit" class="btn">Login</button>
+
+        <div class="register-link">
+            <a href="/signUp">Don't have an account?</a>
+        </div>
+    </form>
+</div>
+
+</x-layout>
