d8d385c9deaf70fb6136c3d9bb378c89e49e5372
Benjamin Lee <benlee007@live.co.uk>
2025-12-05 12:11:35 +0000

    added fillable to basket

--- app/Models/Basket.php
+++ app/Models/Basket.php
@@ -9,7 +9,9 @@ class Basket extends Model
 {
     /** @use HasFactory<\Database\Factories\BasketFactory> */
     use HasFactory;
-    
+    protected $fillable = [
+        'user_id',
+    ];
     public function basket_product() {
         return $this->hasmany(BasketProduct::class);
     }
