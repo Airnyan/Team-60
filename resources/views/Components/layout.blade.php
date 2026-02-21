edfbe71f6ff9356377d27167b5b4bc9029a87924
Kamalpreet <kamalpreetsingh.ks17@gmail.com>
2026-02-21 22:03:28 +0000

    Update the fonts

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -18,7 +18,7 @@
     <!--link to Google Fonts-->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Exo+2:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Exo+2:ital,wght@0,100..900;1,100..900&family=Inter:wght@300;400;600;700&display=swap" rel="stylesheet">
     <!--link to tailwind files-->
     @vite(['resources/css/app.css', 'resources/js/response.js', 'resources/js/app.js','resources/js/chatbot.js'])
 
