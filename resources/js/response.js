31553476adeb0de8c208adfe4c7abc50f97d1797
Kamalpreet <kamalpreetsingh.ks17@gmail.com>
2026-03-09 12:36:58 +0000

    update the chatbot to handle quries and share links to the user

--- resources/js/response.js
+++
@@ -1,8 +0,0 @@
-// This file will handle respones of the chatbot for user
-// Using window global variable so chatbot.js file can access it
-window.responseObj = {
-  hello: "Hey ! How are you doing ?",
-  hey: "Hey! What's Up",
-  today: new Date().toDateString(),
-  time: new Date().toLocaleTimeString(),
-};
