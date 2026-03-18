115ba4628d07aab9050bd024d5b11711d80b0227
Benjamin Lee <benlee007@live.co.uk>
2026-03-18 10:07:23 +0000

    Fixed relationship between user and address tables

---
+++ database/migrations/2026_03_18_092207_update_address_user_fk.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign(['address_id']);
+            $table->dropColumn('address_id');
+        });
+
+        Schema::table('addresses', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id')->nullable()->after('id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedBigInteger('address_id')->nullable()->after('id');
+            $table->foreign('address_id')->references('id')->on('addresses')->onDelete('cascade');
+        });
+
+        Schema::table('addresses', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+            $table->dropColumn('user_id');
+        });
+
+    }
+};
