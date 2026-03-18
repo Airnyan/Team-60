6399a6006c42e6aa6417222dc878831d99b0a5c6
Benjamin Lee <benlee007@live.co.uk>
2026-03-18 10:56:40 +0000

    Adds user id to address on checkout

--- app/Http/Controllers/BasketController.php
+++ app/Http/Controllers/BasketController.php
@@ -139,6 +139,7 @@ class BasketController extends Controller
             'postcode' => 'required|string|max:20',
         ]);
         $address = Address::create([
+            'user_id' => $user->id,
             'address_line_1' => $validated['address1'],
             'address_line_2' => $validated['address2'],
             'postcode' => $validated['postcode'],
